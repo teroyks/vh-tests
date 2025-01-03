1caef75df01b52871dc7acf5d7e173571b746e2e
Tero Ykspetäjä <tero@valohai.com>
2025-01-03 10:36:58 +0200

    ✨ Print execution info to execution log
    Gather some info from the Valohai running environment and log it in the
    execution log.
    
    Add a recipe for running the execution in adhoc mode.

---
+++ execution-info/print_execution_info.py
@@ -0,0 +1,43 @@
+"""Print out some execution-related information."""
+
+import json
+import logging
+from pathlib import Path
+
+import valohai
+from valohai.config import is_running_in_valohai
+from valohai.internals import global_state
+from valohai.internals.guid import get_execution_guid
+from valohai.paths import get_config_path
+
+log = logging.getLogger(__name__)
+
+valohai.prepare(
+    step="exec-info",
+    image="ghcr.io/astral-sh/uv:python3.12-bookworm-slim",
+)
+
+execution_guid = get_execution_guid()
+print(f"Execution GUID: {execution_guid}")
+
+running_in_valohai = is_running_in_valohai()
+print(f"Running in Valohai: {running_in_valohai}")
+
+print(f"Step name: {global_state.step_name}")
+
+print(f"Inputs: {global_state.inputs}")
+print(f"Parameters: {global_state.parameters}")
+
+config_path = Path(get_config_path())
+print(f"Config path: {config_path}")
+try:
+    for item in config_path.iterdir():
+        print(item)
+
+    execution_config_file = config_path / "execution.json"
+    execution_config = json.loads(execution_config_file.read_bytes())
+    print(execution_config)
+except FileNotFoundError:
+    log.warning("⚠️ Config path or execution config does not exist")
+except ValueError:
+    log.warning("⚠️ Error reading config file: not JSON")
--- justfile
+++ justfile
@@ -3,3 +3,7 @@ dev:
 
 run-adhoc-fixed-dataset-version:
     vh execution run --adhoc --title "fixed dataset exec" properties-fixed-dataset-version --nr-of-files=3
+
+run-adhoc-print-execution-info:
+    vh execution run --adhoc --title "execution info" exec-info
+
