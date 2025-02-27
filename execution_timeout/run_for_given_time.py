e42350171505810a426a6a1feb748fa88293ede5
Tero Ykspetäjä <tero@valohai.com>
2025-02-27 12:28:54 +0200

    Add some config debug printing to the script
    Print out execution config and runtime config to the log.

--- execution_timeout/run_for_given_time.py
+++ execution_timeout/run_for_given_time.py
@@ -1,8 +1,11 @@
 """Run an execution for a time given as a parameter."""
 
+import json
 import time
+from pathlib import Path
 
 import valohai
+from valohai.paths import get_config_path
 
 parameters = {
     "duration": 20,  # time to run the script for, in seconds
@@ -14,10 +17,33 @@ valohai.prepare(
 )
 
 
+def get_execution_config() -> dict:
+    config_file = Path(get_config_path()) / "execution.json"
+    try:
+        return json.loads(config_file.read_text())
+    except FileNotFoundError:
+        return {}
+
+
+def get_runtime_config() -> dict:
+    config_file = Path(get_config_path()) / "runtime.json"
+    try:
+        return json.loads(config_file.read_text())
+    except FileNotFoundError:
+        return {}
+
+
 def main():
     duration = int(valohai.parameters("duration").value)
 
+    print("Execution config:")
+    print(json.dumps(get_execution_config(), indent=2))
+
+    print("Runtime config:")
+    print(json.dumps(get_runtime_config(), indent=2))
+
     print(f"Running for {duration} seconds...")
+
     for i in range(duration):
         time.sleep(1)
         print(f"{i + 1}", flush=True)
