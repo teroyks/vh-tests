7f2866d93e38da061e5c0b5f2ccec995e55e448f
Tero Ykspetäjä <tero@valohai.com>
2025-08-19 15:11:22 +0300

    Add demo script for fetching execution metadata
    This fetches the API token unique to the execution -- it can be later
    used for accessing execution-specific data.

---
+++ metadata-without-download/show-input-metadata.py
@@ -0,0 +1,51 @@
+"""Display metadata from an input file without downloading it.
+
+Note: valohai-utils cannot be used in this script because it automatically downloads the file.
+
+When configuring this script in Valohai, add the following to the step configuration:
+```yaml
+inputs:
+  download: on-demand
+```
+
+Read more:
+- https://docs.valohai.com/hc/en-us/articles/31598381149457-On-Demand-Input-Downloads
+- https://valohai.gitbook.io/valohai-docs/projects/data-versioning/dynamic-inputs
+"""
+
+from collections import ChainMap
+import json
+
+# fetch execution config
+
+from pathlib import Path
+
+CONFIG_DIR = Path("/valohai/config")
+if not CONFIG_DIR.exists():
+    print("Running locally, using current directory as config dir.")
+    CONFIG_DIR = Path(".")
+
+
+def parse_metadata(data: dict) -> dict:
+    """Parse metadata from input data.
+
+    Metadata is stored in a list of dictionaries --
+    only the first item is currently in use but this may change in the future.
+    """
+
+    # property metadata is optional – default is a list with one empty dict
+    properties = data.get("metadata", [{}])
+
+    # flatten the list of dictionaries into a single dictionary
+    # [{"key": "value"}, {"key2": "value2"}, {}] -> {"key": "value", "key2": "value2"}
+    return dict(ChainMap(*properties))
+
+
+api_config_json = (CONFIG_DIR / "api.json").read_text()
+api_config = json.loads(api_config_json)
+
+
+_, execution_token = api_config["input_request"]["headers"]["Authorization"].split(
+    " ", 1
+)
+print(f"Execution token: {execution_token}")
