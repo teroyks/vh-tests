cbed14b2dcabbbf746c7839921299659c92c1b2b
Tero Ykspetäjä <tero@valohai.com>
2025-08-21 16:57:36 +0300

    Call API endpoint
    Trying to call the plain API URL to see if the connection is successful.
    In the local dev environment, this doesn't work -- but maybe that is
    just a local issue.

--- call_api/test_api_call.py
+++ call_api/test_api_call.py
@@ -1,7 +1,17 @@
 """Call Valohai API from an execution."""
 
+import httpx
+
 from helpers.valohai import get_api_url
 
 API_URL = get_api_url()
 
 print(f"API URL: {API_URL}")
+
+# Calling the plain API URL should return a list of available endpoints.
+httpx_response = httpx.get(API_URL, timeout=10)
+if httpx_response.status_code == 200:
+    print("API call successful")
+else:
+    print(f"Failed to reach API. Status code: {httpx_response.status_code}")
+    print("Response content:", httpx_response.text)
